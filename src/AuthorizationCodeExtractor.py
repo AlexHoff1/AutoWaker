ab261abae33f8bbf8e50893879dbe9769e82ba76
Alex Hoff <hoffx231@umn.edu>
2017-07-01 22:03:35 -0700

    Successfully extracts the initial authorization code for a user.

--- src/AuthorizationCodeExtractor.py
+++ src/AuthorizationCodeExtractor.py
@@ -1,10 +1,12 @@
 #https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=228FD6&redirect_uri=https://sites.google.com/a/umn.edu/autowaker/&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight
-import base64
-import urllib2
-import urllib
-import webbrowser
 from fitbit.api import Fitbit
 
 def getAuthCode():
-    actual_url = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=228FD6&redirect_uri=https://sites.google.com/a/umn.edu/autowaker/&scope=sleep&expires_in=86400'
-    webbrowser.open(actual_url, new = 1)
+    myfitbit = Fitbit(
+        client_id='228FD6',
+        client_secret='',
+        redirect_url='https://sites.google.com/a/umn.edu/autowaker/',
+        scope = 'sleep',
+        timeout=10)
+    url, auth_code = myfitbit.client.authorize_token_url()
+    return auth_code
