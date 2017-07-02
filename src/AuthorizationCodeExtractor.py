9241347a314c1755ca75a140449b83a55ae38c96
Alex Hoff <hoffx231@umn.edu>
2017-07-02 09:37:15 -0700

    Actually extracting the first access token, refresh token, and other necessary information to get new people on boarded. Uses cherrypy and oauth2 libraries to accomplish this, was copied from an external source and slightly modified so I get what I needed.

--- src/AuthorizationCodeExtractor.py
+++ src/AuthorizationCodeExtractor.py
@@ -1,12 +1,90 @@
-#https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=228FD6&redirect_uri=https://sites.google.com/a/umn.edu/autowaker/&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight
-from fitbit.api import Fitbit
+import cherrypy
+import os
+import sys
+import threading
+import traceback
+import webbrowser
 
-def getAuthCode():
-    myfitbit = Fitbit(
-        client_id='228FD6',
-        client_secret='',
-        redirect_url='https://sites.google.com/a/umn.edu/autowaker/',
-        scope = 'sleep',
-        timeout=10)
-    url, auth_code = myfitbit.client.authorize_token_url()
-    return auth_code
+from base64 import b64encode
+from fitbit.api import Fitbit
+from oauthlib.oauth2.rfc6749.errors import MismatchingStateError, MissingTokenError
+import ClientSecretExtractor
+import TokenGetter
+
+class OAuth2Server:
+    def __init__(self, client_id, client_secret,
+                 redirect_uri='http://127.0.0.1:8080/'):
+        """ Initialize the FitbitOauth2Client """
+        self.success_html = """
+            <h1>You are now authorized to access the Fitbit API!</h1>
+            <br/><h3>You can close this window</h3>"""
+        self.failure_html = """
+            <h1>ERROR: %s</h1><br/><h3>You can close this window</h3>%s"""
+
+        self.fitbit = Fitbit(
+            client_id,
+            client_secret,
+            redirect_uri=redirect_uri,
+            timeout=10,
+        )
+
+    def browser_authorize(self):
+        """
+        Open a browser to the authorization url and spool up a CherryPy
+        server to accept the response
+        """
+        url, _ = self.fitbit.client.authorize_token_url()
+        # Open the web browser in a new thread for command-line browser support
+        threading.Timer(1, webbrowser.open, args=(url,)).start()
+        cherrypy.quickstart(self)
+
+    @cherrypy.expose
+    def index(self, state, code=None, error=None):
+        """
+        Receive a Fitbit response containing a verification code. Use the code
+        to fetch the access_token.
+        """
+        error = None
+        if code:
+            try:
+                self.fitbit.client.fetch_access_token(code)
+            except MissingTokenError:
+                error = self._fmt_failure(
+                    'Missing access token parameter.</br>Please check that '
+                    'you are using the correct client_secret')
+            except MismatchingStateError:
+                error = self._fmt_failure('CSRF Warning! Mismatching state')
+        else:
+            error = self._fmt_failure('Unknown error while authenticating')
+        # Use a thread to shutdown cherrypy so we can return HTML first
+        self._shutdown_cherrypy()
+        return error if error else self.success_html
+
+    def _fmt_failure(self, message):
+        tb = traceback.format_tb(sys.exc_info()[2])
+        tb_html = '<pre>%s</pre>' % ('\n'.join(tb)) if tb else ''
+        return self.failure_html % (message, tb_html)
+
+    def _shutdown_cherrypy(self):
+        """ Shutdown cherrypy in one second, if it's running """
+        if cherrypy.engine.state == cherrypy.engine.states.STARTED:
+            threading.Timer(1, cherrypy.engine.exit).start()
+
+
+if __name__ == '__main__':
+    client_id = '228FD6'
+    client_secret = ClientSecretExtractor.getClientSecret()
+
+    server = OAuth2Server(client_id, client_secret)
+    server.browser_authorize()
+
+    profile = server.fitbit.user_profile_get()
+    for key, value in server.fitbit.client.session.token.items():
+        print('{} = {}'.format(key, value))
+        if key == 'refresh_token':
+            refresh_token = value
+        if key == 'access_token':
+            access_token = value
+    
+    token_getter = TokenGetter.TokenGetter('Tokens.txt')
+    token_getter.setTokens(access_token = access_token, refresh_token = refresh_token)
