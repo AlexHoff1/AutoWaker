c33accc378bf1f5518e289a8428d80e340284369
Alex Hoff <hoffx231@umn.edu>
2017-07-01 17:29:40 -0700

    Adding new tests.

---
+++ test/test_api_handler.py
@@ -0,0 +1,12 @@
+import unittest
+import datetime
+import APIHandler
+
+class TestAPIHandler(unittest.TestCase):
+
+    def test_apihandler(self):
+        self.assertEqual(1, 1)
+
+        
+if __name__ == '__main__':
+    unittest.main()
