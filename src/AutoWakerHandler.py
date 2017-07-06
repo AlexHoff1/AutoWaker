dface29c2cbf34a00c96a488b089e7b37598a0b8
Alex Hoff <hoffx231@umn.edu>
2017-07-05 20:19:11 -0700

    Adding something for the backend to return the start time so I can make this current sourcecode a backend for the java based app.

---
+++ src/AutoWakerHandler.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import sys
+
+from APIHandler import APIHandler
+from ConfigHandler import getPath
+from DataHandler import DataHandler
+from LogCreator import setupLogger
+from SleepChecker import SleepChecker
+from TimeHandler import today, now, startCheckTime, endCheckTime, stallAction
+from TokenGetter import TokenGetter
+from WakeUpCaller import WakeUpCaller
+
+
+#Set the proper path
+os.chdir(getPath())
+
+today_as_dt = today()
+
+
+#Get and write the tokens from here
+s = [getPath(), 'Data', 'MyData_' + today_as_dt + '.txt']
+OutFile = os.path.join(*s)
+
+def getUser():
+    return '-'
+
+user = getUser()
+#This is the Fitbit URL to use for the API call
+FitbitURL = "https://api.fitbit.com/1/user/" + user + "/sleep/date/" + today_as_dt + ".json"
+
+def getStartTime(): 
+    
+    LOG = setupLogger()
+    LOG.info('STARTING')
+    
+    # wait until we should start. Make this into a class or method?
+    while now()>endCheckTime() or now()<startCheckTime():
+        LOG.info('Pinging every 120 seconds to check if the person should be asleep.')
+        stallAction(120)
+    
+    key_getter = TokenGetter(os.path.join(getPath(),'tokens.txt'))
+    api_handler = APIHandler(FitbitURL, OutFile, key_getter)
+    data_handler = DataHandler()
+    
+    #Make the API call
+    APIResponse = api_handler.cancelIfAPICallBad()
+    sleep_checker = SleepChecker(OutFile)
+    LOG.info('starting the cycle... Forever.')
+    while sleep_checker.isAwake():
+        stallAction(600)  #Check every 10 minutes.
+        APIResponse = api_handler.cancelIfAPICallBad()
+        sleeping, start_time = data_handler.getSleepStartTime(APIResponse)
+        
+    #Sleeping is true now.
+    LOG.info('You started sleeping at ' + str(start_time) + ' today.')
+    wake_up = WakeUpCaller()
+    return wake_up.calculateWakeTime(start_time);
+
+
+
+if __name__ == '__main__':
+    main()
