8c2175bb300aa18f6e474b11f1f4532f5ed6361a
Alex Hoff <hoffx231@umn.edu>
2017-07-05 21:21:24 -0700

    Adding a test for authorization code extractor, only problem is it's not isolated nor repeatable :\.

---
+++ test/test_authorization_code_handler.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+
+import AuthorizationCodeExtractor
+from TokenGetter import TokenGetter
+
+class TestAuthorizationCodeExtractor(unittest.TestCase):
+
+    def test_no_data(self):
+        token_getter = TokenGetter('tokens.txt')
+        access_token, refresh_token = token_getter.getTokens()
+        AuthorizationCodeExtractor.setupTokens()
+        new_access_token, new_refresh_token = token_getter.getTokens()
+        self.assertTrue(new_access_token!=access_token)
+        self.assertTrue(new_refresh_token!=refresh_token)
+                
+if __name__ == '__main__':
+    unittest.main()
