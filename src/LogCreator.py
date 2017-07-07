75e98884ff36209c98b4c8227d682f22ba92e63d
Alex Hoff <hoffx231@umn.edu>
2017-07-06 20:51:07 -0700

    Clarifying how this works.

--- src/LogCreator.py
+++ src/LogCreator.py
@@ -4,12 +4,18 @@ import os
 from ConfigHandler import getPath
 from TimeHandler import today
 
+
+#  Puts log into a relative location, and adds a day stamp.
 def setupLogger():
     today_as_dt = today()
     logging.basicConfig(filename='sleepDataLogs.log',level=logging.INFO, filemode ='w')
+    
+    # ~/Logs/sleepLogs*date*.log
     relative_log_location = [getPath(), 'Logs', 'sleepLogs' + today_as_dt + '.log']
     hdlr = logging.FileHandler(os.path.join(*relative_log_location))
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    
+    
     hdlr.setFormatter(formatter)
     hdlr.setLevel(logging.INFO)
     LOG = logging.getLogger(name = "autoWaker")
