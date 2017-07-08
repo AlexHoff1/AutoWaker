ab2c9c2f3cd34ffaf02cffb7bb7071d7551fd0b8
Alex Hoff <hoffx231@umn.edu>
2017-07-07 23:03:29 -0700

    Cleaning up the case where Config might have been dumped to.

--- src/ConfigHandler.py
+++ src/ConfigHandler.py
@@ -1,6 +1,6 @@
 #This will be generalized later.
 def getPath():
     config_file = open('Config.txt')
-    relative_path = config_file.read().strip()
+    relative_path = config_file.readline().strip()
     config_file.close()
     return relative_path
