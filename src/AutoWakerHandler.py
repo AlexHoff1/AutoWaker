060bc027934b3f3b8f08627587256eae21679eb1
Alex Hoff <hoffx231@umn.edu>
2017-07-17 19:09:15 -0700

    Trying to return the proper time from calling the backend. WIP.

--- src/AutoWakerHandler.py
+++ src/AutoWakerHandler.py
@@ -22,42 +22,37 @@ today_as_dt = today()
 s = [getPath(), 'Data', 'MyData_' + today_as_dt + '.txt']
 OutFile = os.path.join(*s)
 
-def getUser():
-    return '-'
-
-user = getUser()
-#This is the Fitbit URL to use for the API call
-FitbitURL = "https://api.fitbit.com/1/user/" + user + "/sleep/date/" + today_as_dt + ".json"
-
-def getStartTime(): 
-    
-    LOG = setupLogger()
-    LOG.info('STARTING')
-    
-    # wait until we should start. Make this into a class or method?
-    while now()>endCheckTime() or now()<startCheckTime():
-        LOG.info('Pinging every 120 seconds to check if the person should be asleep.')
-        stallAction(120)
-    
-    key_getter = TokenGetter(os.path.join(getPath(),'tokens.txt'))
-    api_handler = APIHandler(FitbitURL, OutFile, key_getter)
-    data_handler = DataHandler()
-    
-    #Make the API call
-    APIResponse = api_handler.cancelIfAPICallBad()
-    sleep_checker = SleepChecker(OutFile)
-    LOG.info('starting the cycle... Forever.')
-    while sleep_checker.isAwake():
-        stallAction(600)  #Check every 10 minutes.
-        APIResponse = api_handler.cancelIfAPICallBad()
-        sleeping, start_time = data_handler.getSleepStartTime(APIResponse)
+class ServerRequestHandler():
+    def __init__(self, user = None, date = None):
+        self.user_ = user
+        self.date_ = date
         
-    #Sleeping is true now.
-    LOG.info('You started sleeping at ' + str(start_time) + ' today.')
-    wake_up = WakeUpCaller()
-    return wake_up.calculateWakeTime(start_time);
+    def getUser(self):
+        return self.user_
+    
+    
+    def getWakeTime(self): 
+        user = self.getUser()
+        FitbitURL = "https://api.fitbit.com/1/user/" + user + "/sleep/date/" + today_as_dt + ".json"
+        return '06:00:00'
+        LOG = setupLogger()
+        LOG.info('STARTING')
 
-
-
-if __name__ == '__main__':
-    main()
+        key_getter = TokenGetter(os.path.join(getPath(),'tokens.txt'))
+        api_handler = APIHandler(FitbitURL, OutFile, key_getter)
+        data_handler = DataHandler()
+        
+        #Make the API call
+        APIResponse = api_handler.cancelIfAPICallBad()
+        sleep_checker = SleepChecker(OutFile)
+        LOG.info('starting the cycle... Forever.')
+        if sleep_checker.isAwake():
+            return '06:00:00'
+        else:            
+            APIResponse = api_handler.cancelIfAPICallBad()
+            sleeping, start_time = data_handler.getSleepStartTime(APIResponse)
+            
+        #Sleeping is true now.
+        LOG.info('You started sleeping at ' + str(start_time) + ' today.')
+        wake_up = WakeUpCaller()
+        return wake_up.calculateWakeTime(start_time);
