deaaf303580fe1ceb7e644d8afbc0065521c0f85
Alex Hoff <alex@ums-10-155-181-255.wireless.umn.edu>
2017-11-01 11:24:39 -0500

    Adding some extra error messages.

--- src/ClientSecretExtractor.py
+++ src/ClientSecretExtractor.py
@@ -4,7 +4,10 @@ from ConfigHandler import getPath
 
 def getClientSecret():
     relative_location = [getPath(), 'Data', 'ClientSecret.txt']
-    fid = open(os.path.join(*relative_location))
-    client_secret = fid.read()
-    fid.close()
+    try:
+        fid = open(os.path.join(*relative_location))
+        client_secret = fid.read()
+        fid.close()
+    except:
+        raise IOError('The client secret file did not exist.')
     return client_secret.strip()
