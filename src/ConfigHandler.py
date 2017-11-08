9b8e638e2410a3f0bbd436d11e08e41bfadb2749
Alex Hoff <alexhoff1online@gmail.com>
2017-11-08 10:28:33 -0600

    Adding logging to ConfigHandler for debug and error checks.

--- src/ConfigHandler.py
+++ src/ConfigHandler.py
@@ -1,10 +1,14 @@
+import logging
 import os
+
+LOG = logging.getLogger(name="autoWaker")
 
 #Returns the path contained in the config file.
 def getPath():
     config_file = open(getRelativeLocation())
     relative_path = config_file.readline().strip()
     config_file.close()
+    LOG.info('Relative path read from file: ' + relative_path)
     return relative_path
 
 #Moves back a directory and grabs the config file.
@@ -15,4 +19,4 @@ def getRelativeLocation():
         supposed_config_string_unjoined = supposed_config_path[0:len(supposed_config_path)-1]+['Config.txt']
         return '/'.join(supposed_config_string_unjoined)
     except:
-        print('Tossed.')
+        LOG.error('Tossed when trying to get the relative location of Config.txt.')
