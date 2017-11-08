63d069525bddf24367f7cb6b8ba7b866941f21ac
Alex Hoff <alexhoff1online@gmail.com>
2017-11-08 10:24:58 -0600

    Adding more relative pathing and fixing the config file since it wasn't generalized with OS yet.

--- src/ConfigHandler.py
+++ src/ConfigHandler.py
@@ -1,6 +1,18 @@
-#This will be generalized later.
+import os
+
+#Returns the path contained in the config file.
 def getPath():
-    config_file = open('Config.txt')
+    config_file = open(getRelativeLocation())
     relative_path = config_file.readline().strip()
     config_file.close()
     return relative_path
+
+#Moves back a directory and grabs the config file.
+def getRelativeLocation():
+    try:
+        cwd = os.getcwd()
+        supposed_config_path = cwd.split('/')
+        supposed_config_string_unjoined = supposed_config_path[0:len(supposed_config_path)-1]+['Config.txt']
+        return '/'.join(supposed_config_string_unjoined)
+    except:
+        print('Tossed.')
